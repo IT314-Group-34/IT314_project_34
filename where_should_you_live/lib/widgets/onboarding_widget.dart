354d9f45b8d5b2e69760547e59567ea9b2d3fbc3
Aditya Nawal <nawaladitya7.2@gmail.com>
2023-04-07 12:29:43 +0530

    Add onboarding widget

---
+++ where_should_you_live/lib/widgets/onboarding_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:where_should_you_live/widgets/onboarding_widget.dart';
+
+class OnBoardingPageWidget extends StatelessWidget {
+  const OnBoardingPageWidget({
+    Key? key,
+    required this.model,
+  }) : super(key: key);
+  final OnBoardingModel model;
+
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      padding: const EdgeInsets.all(tDefaultSize),
+      color: model.bgColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image(
+            image: AssetImage(model.image),
+            height: size.height * 0.45,
+          ),
+          Column(
+            children: [
+              Text(
+                model.title,
+                style: Theme.of(context).textTheme.headline3,
+              ),
+              Text(
+                model.subTitle,
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+          Text(
+            model.counterText,
+            style: Theme.of(context).textTheme.headline6,
+          ),
+          const SizedBox(
+            height: 80.0,
+          )
+        ],
+      ),
+    );
+  }
+}
