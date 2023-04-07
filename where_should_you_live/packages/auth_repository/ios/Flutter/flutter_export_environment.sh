ba9ac5aa036abedee1ff748c2615a7a12dbea567
Aditya Nawal <nawaladitya7.2@gmail.com>
2023-04-07 12:15:56 +0530

    System Generated files

---
+++ where_should_you_live/packages/auth_repository/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\nawal\flutter"
+export "FLUTTER_APPLICATION_PATH=G:\My Drive\Academics\Semester 6\IT314 SWE\Project\IT314_project_34\where_should_you_live\packages\auth_repository"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.0.1"
+export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
