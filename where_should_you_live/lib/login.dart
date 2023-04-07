bedc38843c7be7210da675e5240d8e515ab5fe5f
Parth2301 <202001450@daiict.ac.in>
2023-04-07 13:30:58 +0530

    made login page responsive and error handling

--- where_should_you_live/lib/login.dart
+++ where_should_you_live/lib/login.dart
@@ -1,117 +1,168 @@
 import 'package:flutter/material.dart';
 
+import 'auth.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(const Login());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<Login> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends State<Login> {
+  late TextEditingController _emailController;
+  late TextEditingController _passwordController;
+  final formKey = GlobalKey<FormState>();
+  @override
+  initState() {
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   bool passenable = true;
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.white,
-          leading: const Text(
-            'Feature',
-            style: TextStyle(color: Colors.grey),
-          ),
+          title: const Text('Login Screen'),
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Text(
-              'Login',
-              style: TextStyle(
-                fontSize: 35,
-                color: Colors.black,
-                //fontWeight: FontWeight.bold,
-              ),
-            ),
             Padding(
-              padding: const EdgeInsets.symmetric(vertical: 30),
+              padding: EdgeInsets.symmetric(vertical: screenHeight * 0.05),
               child: Form(
+                  key: formKey,
                   child: Column(
-                children: [
-                  Center(
-                    child: MaterialButton(
-                      minWidth: 150,
-                      onPressed: () {},
-                      color: Colors.blue,
-                      textColor: Colors.white,
-                      child: const Text('Log in With Google'),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  const SizedBox(
-                    width: 300,
-                    child: Divider(
-                      color: Colors.grey,
-                      height: 25,
-                      thickness: 1,
-                      indent: 5,
-                      endIndent: 5,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  SizedBox(
-                    width: 300,
-                    child: TextFormField(
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: const InputDecoration(
-                          labelText: 'Email Address',
-                          hintText: 'Enter Email Id',
-                          prefixIcon: Icon(Icons.email),
-                          border: OutlineInputBorder(),
+                    children: [
+                      Center(
+                        child: MaterialButton(
+                          minWidth: screenWidth * 0.3,
+                          onPressed: () {
+                            signInWithGoogle(context);
+                          },
+                          color: Colors.blue,
+                          height: MediaQuery.of(context).size.height * 0.07,
+                          textColor: Colors.white,
+                          child: const Text('Log in With Google'),
                         ),
-                        onChanged: (String value) {},
-                        validator: (value) {
-                          return value!.isEmpty
-                              ? 'Please Enter Email Id'
-                              : null;
-                        }),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  SizedBox(
-                    width: 300,
-                    child: TextFormField(
-                      obscureText:
-                          passenable, //if passenable == true, show **, else show password character
-                      decoration: const InputDecoration(
-                        hintText: "Password",
-                        labelText: "Password",
-                        prefixIcon: Icon(Icons.lock),
-                        border: OutlineInputBorder(),
-                        //eye icon if passenable = true, else, Icon is ***__
                       ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  MaterialButton(
-                    minWidth: 100,
-                    onPressed: () {},
-                    color: Colors.blue,
-                    textColor: Colors.white,
-                    child: const Text('Log in'),
-                  ),
-                ],
-              )),
+                      SizedBox(
+                        height: screenHeight * 0.03,
+                      ),
+                      SizedBox(
+                        width: screenWidth * 0.4,
+                        child: const Divider(
+                          color: Colors.black,
+                          height: 25,
+                          thickness: 2,
+                          indent: 5,
+                          endIndent: 5,
+                        ),
+                      ),
+                      SizedBox(
+                        height: screenHeight * 0.03,
+                      ),
+                      SizedBox(
+                        width: screenWidth * 0.4,
+                        child: TextFormField(
+                            keyboardType: TextInputType.emailAddress,
+                            controller: _emailController,
+                            decoration: const InputDecoration(
+                              labelText: 'Email Address',
+                              hintText: 'Enter Email Id',
+                              prefixIcon: Icon(Icons.email),
+                              border: OutlineInputBorder(),
+                            ),
+                            onChanged: (String value) {},
+                            validator: (value) {
+                              return value!.isEmpty
+                                  ? 'Please Enter Email Id'
+                                  : null;
+                            }),
+                      ),
+                      SizedBox(
+                        height: screenHeight * 0.03,
+                      ),
+                      SizedBox(
+                        width: screenWidth * 0.4,
+                        child: TextFormField(
+                            obscureText:
+                                passenable, //if passenable == true, show **, else show password character
+                            controller: _passwordController,
+                            decoration: const InputDecoration(
+                              hintText: "Password",
+                              labelText: "Enter Password",
+                              prefixIcon: Icon(Icons.lock),
+                              border: OutlineInputBorder(),
+                              //eye icon if passenable = true, else, Icon is ***__
+                            ),
+                            onChanged: (String value) {},
+                            validator: (value) {
+                              return value!.isEmpty
+                                  ? 'Please Enter password'
+                                  : null;
+                            }),
+                      ),
+                      SizedBox(
+                        height: screenHeight * 0.03,
+                      ),
+                      MaterialButton(
+                        minWidth: screenWidth * 0.3,
+                        onPressed: () async {
+                          // Validate returns true if the form is valid, or false otherwise.
+                          if (formKey.currentState!.validate()) {
+                            // If the form is valid, display a snackbar. In the real world,
+                            // you'd often call a server or save the information in a database.
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('Processing Data')),
+                            );
+                            await loginWithEmail(context, _emailController.text,
+                                _passwordController.text);
+                          }
+                        },
+                        color: Colors.blue,
+                        height: MediaQuery.of(context).size.height * 0.07,
+                        textColor: Colors.white,
+                        child: const Text('Log in'),
+                      ),
+                      SizedBox(
+                        height: screenHeight * 0.03,
+                      ),
+                      TextButton(
+                          onPressed: () => {
+                                Navigator.pushReplacementNamed(
+                                    context, '/forgotPassword'),
+                              },
+                          child: Text('Forgot Password?')),
+                      SizedBox(
+                        height: screenHeight * 0.03,
+                      ),
+                      TextButton(
+                          onPressed: () => {
+                                Navigator.pushReplacementNamed(
+                                    context, '/signup'),
+                              },
+                          child: Text('Don\'t have an account? Sign up.'))
+                    ],
+                  )),
             )
           ],
         ),
--- where_should_you_live/lib/main.dart
+++ where_should_you_live/lib/main.dart
@@ -1,7 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:where_should_you_live/firebase_options.dart';
+import 'forgotPassword.dart';
+import './signup.dart';
+import './login.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MaterialApp(
+    home: const MyHomePage(),
+    routes: {
+      '/signup': (context) => const signUp(),
+      '/login': (context) => const Login(),
+      '/home': (context) => const MyHomePage(),
+      '/forgotPassword': (context) => ForgotPasswordScreen(),
+    },
+  ));
 }
 
 class MainApp extends StatelessWidget {
@@ -9,10 +24,57 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: ElevatedButton(
+            onPressed: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const signUp()),
+              )
+            },
+            child: const Text('Sign Up'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Homepage'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const signUp()),
+                )
+              },
+              child: const Text('Sign Up'),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Login()),
+                ),
+              },
+              child: const Text('Login'),
+            ),
+          ],
         ),
       ),
     );
