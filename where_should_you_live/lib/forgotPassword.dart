e84a0e1d8ff87e5789e5adf35b2748df92feaea3
Parth2301 <202001450@daiict.ac.in>
2023-04-07 12:57:55 +0530

    added new functionality forgot passsword

---
+++ where_should_you_live/lib/forgotPassword.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class ForgotPasswordScreen extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Forgot Password'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(vertical: screenHeight * 0.05),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(labelText: 'Email'),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter your email address';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(
+                height: screenHeight * 0.05,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    try {
+                      await FirebaseAuth.instance.sendPasswordResetEmail(
+                          email: _emailController.text.trim());
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Password reset email sent')));
+                    } on FirebaseAuthException catch (e) {
+                      String? errorMessage = e.message;
+                      ScaffoldMessenger.of(context)
+                          .showSnackBar(SnackBar(content: Text(errorMessage!)));
+                    }
+                  }
+                },
+                child: const Text('Reset Password'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
