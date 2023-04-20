7cb59150d2c652fa6de98e6ed6725ace3dae6388
atmshah16 <aatmanshah020802@gmail.com>
2023-04-21 02:06:50 +0530

    Updated boiler plate onboarding pages

--- where_should_you_live/lib/src/constants/text_strings.dart
+++ where_should_you_live/lib/src/constants/text_strings.dart
@@ -1,10 +1,12 @@
 //Onboarding Text
 
-const String tOnboardingTitle1 = 'Welcome to Where Should You Live?';
-const String tOnboardingTitle2 = 'Find out where you should live';
+const String tOnboardingTitle1 = 'Welcome to "Where Should You Live?" ';
+const String tOnboardingTitle2 =
+    'Now your dream home is just a few clicks away';
 const String tOnboardingTitle3 = 'Find out where you should live';
-const String tOnboardingSubTitle1 = 'This app will help you find out where you should live.';
-const String tOnboardingSubTitle2 = 'Answer the questions and find out!';
+
+const String tOnboardingSubTitle1 = 'It will help you find your dream place.';
+const String tOnboardingSubTitle2 = 'Now find the place that best suits you';
 const String tOnboardingSubTitle3 = 'Answer the questions and find out!';
 
 //Onboarding Counter Text
