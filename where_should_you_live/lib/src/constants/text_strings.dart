3108a84bc349673af3459058aa6e828041de7bf7
Aditya Nawal <nawaladitya7.2@gmail.com>
2023-04-21 01:13:44 +0530

    Added boiler plate text

---
+++ where_should_you_live/lib/src/constants/text_strings.dart
@@ -0,0 +1,13 @@
+//Onboarding Text
+
+const String tOnboardingTitle1 = 'Welcome to Where Should You Live?';
+const String tOnboardingTitle2 = 'Find out where you should live';
+const String tOnboardingTitle3 = 'Find out where you should live';
+const String tOnboardingSubTitle1 = 'This app will help you find out where you should live.';
+const String tOnboardingSubTitle2 = 'Answer the questions and find out!';
+const String tOnboardingSubTitle3 = 'Answer the questions and find out!';
+
+//Onboarding Counter Text
+const String tOnboardingCounterText1 = '1/3';
+const String tOnboardingCounterText2 = '2/3';
+const String tOnboardingCounterText3 = '3/3';
