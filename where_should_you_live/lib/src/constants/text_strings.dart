4e22a60eb844ef84f0cde79374d35bb1b6fcd764
atmshah16 <aatmanshah020802@gmail.com>
2023-04-21 02:22:14 +0530

    Onboarding subtitle update

--- where_should_you_live/lib/src/constants/text_strings.dart
+++ where_should_you_live/lib/src/constants/text_strings.dart
@@ -3,11 +3,11 @@
 const String tOnboardingTitle1 = 'Welcome to "Where Should You Live?" ';
 const String tOnboardingTitle2 =
     'Now your dream home is just a few clicks away';
-const String tOnboardingTitle3 = 'Find out where you should live';
+const String tOnboardingTitle3 = 'The best place can be yours ';
 
 const String tOnboardingSubTitle1 = 'It will help you find your dream place.';
 const String tOnboardingSubTitle2 = 'Now find the place that best suits you';
-const String tOnboardingSubTitle3 = 'Answer the questions and find out!';
+const String tOnboardingSubTitle3 = 'Log in to find out ';
 
 //Onboarding Counter Text
 const String tOnboardingCounterText1 = '1/3';
