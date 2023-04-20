4ec9cb837e21ec2817c7d088b58d71eacd995dd6
Aditya Nawal <nawaladitya7.2@gmail.com>
2023-04-21 01:11:28 +0530

    Updating main.dart with Onboarding flow

--- where_should_you_live/lib/main.dart
+++ where_should_you_live/lib/main.dart
@@ -1,69 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:where_should_you_live/utils/theme/theme.dart';
-void main() => runApp(const App());
+import 'package:where_should_you_live/src/utils/theme/theme.dart';
+import 'package:where_should_you_live/src/features/authentication/screens/onboarding/onboarding_screen.dart';
 
-
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
-
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-      themeMode: ThemeMode.system,
-      home: AppHome(),
-    );
-  }
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(_buildMaterialApp());
 }
 
-
-class AppHome extends StatelessWidget {
-  const AppHome({Key? key}) : super(key: key);
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: const Text(".appable/"),
-          leading: const Icon(Icons.ondemand_video)),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add_shopping_cart_outlined),
-        onPressed: () {},
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: ListView(
-          children: [
-            Text(
-              "Heading",
-              style: Theme.of(context).textTheme.headline2,
-            ),
-            Text(
-              "Sub-heading",
-              style: Theme.of(context).textTheme.subtitle2,
-            ),
-            Text(
-              "Paragraph",
-              style: Theme.of(context).textTheme.bodyText1,
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("Elevated Button"),
-            ),
-            OutlinedButton(
-              onPressed: () {},
-              child: const Text("Outlined Button"),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Image(image: AssetImage("assets/images/books.png")),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+MaterialApp _buildMaterialApp() {
+  return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Where Should You Live?',
+    theme: TAppTheme.lightTheme,
+    darkTheme: TAppTheme.darkTheme,
+    home: const OnBoardingScreen(),
+  );
 }
