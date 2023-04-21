e279222fa52f51861eec17ce40270a848f6893fe
atmshah16 <aatmanshah020802@gmail.com>
2023-04-22 00:56:42 +0530

    Addition of the password button

--- where_should_you_live/lib/login_page.dart
+++ where_should_you_live/lib/login_page.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:where_should_you_live/src1/constants/image_strings.dart';
 import 'package:where_should_you_live/src1/constants/sizes.dart';
 import 'package:where_should_you_live/src1/constants/text_strings.dart';
-//import '../assets/constants/image_strings.dart';
-//import '../assets/constants/sizes.dart';
-//import '../assets/constants/text_strings.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -30,7 +27,8 @@ class LoginScreen extends StatelessWidget {
                     style: Theme.of(context).textTheme.bodySmall),
                 Form(
                   child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 20.0),
+                    padding:
+                        const EdgeInsets.symmetric(vertical: tFormheight - 10),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
@@ -41,13 +39,20 @@ class LoginScreen extends StatelessWidget {
                               hintText: tEmail,
                               border: OutlineInputBorder()),
                         ),
+                        const SizedBox(height: tFormheight - 20),
                         TextFormField(
                           decoration: const InputDecoration(
-                              prefixIcon: Icon(Icons.person_outline_outlined),
-                              labelText: tPassword,
-                              hintText: tPassword,
-                              border: OutlineInputBorder()),
+                            prefixIcon: Icon(Icons.fingerprint),
+                            labelText: tPassword,
+                            hintText: tPassword,
+                            border: OutlineInputBorder(),
+                            suffixIcon: IconButton(
+                              onPressed: null,
+                              icon: Icon(Icons.remove_red_eye_sharp),
+                            ),
+                          ),
                         ),
+                        const SizedBox(height: tFormheight - 20),
                       ],
                     ),
                   ),
