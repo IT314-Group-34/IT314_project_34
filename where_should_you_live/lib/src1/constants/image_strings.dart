dd39b6479ee30becd3a6332f092c45834800eef5
atmshah16 <aatmanshah020802@gmail.com>
2023-04-21 19:55:50 +0530

    Adding of Login screen

---
+++ where_should_you_live/lib/src1/constants/image_strings.dart
@@ -0,0 +1,7 @@
+const String tOnBoardingImage1 =
+    'assets/images/on_boarding_images/onboarding_1.png';
+const String tOnBoardingImage2 =
+    'assets/images/on_boarding_images/onboarding_2.png';
+const String tOnboardingImage3 =
+    'assets/images/on_boarding_images/onboarding_3.png';
+const String tWelcomeScreenImage = 'lib/assets/images/welcome_2.jpg';
