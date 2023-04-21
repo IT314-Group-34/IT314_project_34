e139114704fb001c22122365d3fb457422bd90b5
smitbhavsar-18 <smitbhavsar1863@gmail.com>
2023-04-21 17:51:49 +0530

    Fornt end for Homepage

--- where_should_you_live/lib/homepage.dart
+++ where_should_you_live/lib/homepage.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   Widget func({required String url, required String value}) {
