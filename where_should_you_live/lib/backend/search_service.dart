d08c554fcc8e8faad271dd739edb1631b7de580b
Aditya Nawal <nawaladitya7.2@gmail.com>
2023-04-24 23:54:17 +0530

    Update search_service to include filters

--- where_should_you_live/lib/backend/search_service.dart
+++ where_should_you_live/lib/backend/search_service.dart
@@ -1,25 +1,44 @@
+// search_service.dart
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+enum ExerciseFilter {
+  filter_1,
+  filter_2,
+  filter_3,
+  filter_4,
+  filter_5,
+  filter_6,
+  filter_7,
+  filter_8,
+  filter_9
+}
+
 class SearchService {
-  searchByCriteria(String criteria, {int minRating = 0, int maxRating = 10}) {
-    return FirebaseFirestore.instance
+  searchByCriteria(String criteria,
+      {int minRating = 0,
+      int maxRating = 10,
+      List<ExerciseFilter> filters = const []}) {
+    Query query = FirebaseFirestore.instance
         .collection('neighbourhoods')
         .where('criteria', isEqualTo: criteria)
         .where('rating', isGreaterThanOrEqualTo: minRating)
-        .where('rating', isLessThanOrEqualTo: maxRating)
-        .get();
+        .where('rating', isLessThanOrEqualTo: maxRating);
+
+    // Apply the filters to the query
+    for (final filter in filters) {
+      switch (filter) {
+        case ExerciseFilter.filter_1:
+          // Add a where clause to the query for filter_1
+          query = query.where('filter_1', isEqualTo: true);
+          break;
+        case ExerciseFilter.filter_2:
+          // Add a where clause to the query for filter_2
+          query = query.where('filter_2', isEqualTo: true);
+          break;
+        // Add additional cases here for the other filters
+      }
+    }
+
+    return query.get();
   }
 }
-
-// In your widget
-// ...
-final SearchService _searchService = SearchService();
-List<DocumentSnapshot> _neighbourhoods = [];
-
-void _searchNeighbourhoods(String criteria) async {
-  final QuerySnapshot result = await _searchService.searchByCriteria(criteria);
-  setState(() {
-    _neighbourhoods = result.docs;
-  });
-}
-// ...
