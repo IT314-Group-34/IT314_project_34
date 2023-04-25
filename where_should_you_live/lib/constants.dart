90a619a833b0358162e6328453f18103a6222cf9
RohanChampaneri11 <rohanchampaneri11@gmail.com>
2023-04-25 11:30:39 +0530

    added theme for profile page

---
+++ where_should_you_live/lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Constants 
+{
+  static const Color appPurpleColor = Color.fromRGBO(29, 9, 93, 1.0);
+  static const Color appLightPurpleColor = Color.fromRGBO(232, 221, 252, 1.0);
+}
