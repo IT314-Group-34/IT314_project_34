198282be3569d94b0497d2ae79a93644e1a14468
Aditya Nawal <nawaladitya7.2@gmail.com>
2023-04-28 06:56:38 +0530

    Fix fatal error of initialisation

--- where_should_you_live/lib/userProvider.dart
+++ where_should_you_live/lib/userProvider.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class UserProvider with ChangeNotifier {
   late String _email;
 
+  UserProvider() {
+    _email = '';
+  }
+
   String get email => _email;
 
   set email(String value) {
